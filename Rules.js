76619581cb72a2d01f1ec49bac4d6458cac8d70a
f-society <42274759+H-f-society@users.noreply.github.com>
2020-02-03 19:04:54 +0800

    Update Rules.js

--- Rules.js
+++ Rules.js
@@ -2,252 +2,153 @@
 * @Author: root
 * @Date:   2020-01-30 23:30:09
 * @Last Modified by:   root
-* @Last Modified time: 2020-02-01 03:07:33
+* @Last Modified time: 2020-02-03 19:02:04
 */
-var foodPS = new Array();
-var MoveDirection = [1, 0];
-var Snake = function() {
-	var body = new Array();
+var GameConfig = function() {
+	this.MapSize   = 50;
+	this.MoveDires = [1, 0];
 }
-Snake.prototype.InitSnakeBody = function() { 
-	let arr = [[1, 0], [0, 0]];
-	return body = arr;
+GameConfig.prototype.updateMoveDires = function(x, y) {
+	this.MoveDires = [x, y];
 }
-Snake.prototype.getSnakeBody = function() {
-	//console.log(body);
-	return body;
+var Snake = function(n) {
+	this.Body = new Array();
+	this.Food = new Array();
+	this.GameMap;
+	this.getSnakeBody = function() {
+		return this.Body;
+	}	
+	var config = new GameConfig();
+	var draw   = new Draw();
+}
+Snake.prototype.InitGameMap = function(MapSize) {
+	this.GameMap = new Array(MapSize);
+	for(let i=0; i<MapSize; i++) {
+		this.GameMap[i] = new Array(MapSize);
+		this.GameMap[i].fill("-");
+	}
+}
+Snake.prototype.InitSnakeBody = function() {
+	return this.Body = [[1, 0], [0, 0]];
+}
+Snake.prototype.InitSnake = function() {
+	let Body = this.getSnakeBody();
+	this.GameMap[Body[0][0]][Body[0][1]] = "*"; 
+	draw.drawRole(Body[0][0]+0.5, Body[0][1]+0.5, 5, "red");
+	for(let i=1; i<Body.length; i++) {
+		draw.drawRole(Body[i][0]+0.5, Body[i][1]+0.5, 5, "blue");
+		this.GameMap[Body[i][0]][Body[i][1]] = "*"; 
+	}
+}
+Snake.prototype.createFood = function() {
+	this.Food[0] = parseInt((Math.random()*config.MapSize).toString(10));
+	this.Food[1] = parseInt((Math.random()*config.MapSize).toString(10));
+	if(this.GameMap[this.Food[0]][this.Food[1]] == "-") {
+		this.GameMap[this.Food[0]][this.Food[1]] = "0";
+		draw.drawRole(this.Food[0]+0.5, this.Food[1]+0.5, 5, "yellow");
+	}else
+		this.createFood();
+}
+Snake.prototype.reMap = function(newHead, clearTail) {
+	this.GameMap[newHead[0]][newHead[1]]   = "*";
+	this.GameMap[clearTail[0]][clearTail[1]] = "-";	
 }
 Snake.prototype.MoveSnake = function(map, direction) {
-	var head = body[0];
-	//console.log(direction, head);
+	var head   = this.Body[0];
 	switch(direction) {
-		case "up"	: 
-			MoveDirection[0] = 0;
-			MoveDirection[1] = -1;
-			if(!Transboundary(map, head[0], head[1]-1) || map[head[0]][head[1]-1] == "*") 
-				console.log("Game Over!");
-			if (Transboundary(map, head[0], head[1]-1) &&
-				map[head[0]][head[1]-1] == "-" ||
-				map[head[0]][head[1]-1] == "0" ) {
-				var bodyEnd = body.pop();
-				eatFood(head[0], head[1]-1, bodyEnd);
-				body.unshift(new Array(head[0], head[1]-1));
-				reDrawSnake(body[0], head, bodyEnd);
-			}
-			if(direction == "down") direction = "up";
-			break;
-		case "down"	: 
-			MoveDirection[0] = 0;
-			MoveDirection[1] = 1;
-			if(!Transboundary(map, head[0], head[1]+1) || map[head[0]][head[1]+1] == "*") 
-				console.log("Game Over!");
-			if(Transboundary(map, head[0], head[1]+1)&&
-				map[head[0]][head[1]+1] == "-" ||
-				map[head[0]][head[1]+1] == "0" ) {
-				var bodyEnd = body.pop();
-				eatFood(head[0], head[1]+1, bodyEnd);
-				body.unshift(new Array(head[0], head[1]+1));
-				reDrawSnake(body[0], head, bodyEnd);
-			}
-			if(direction == "up") direction = "down";
-			break;
-		case "left" : 
-			if(!Transboundary(map, head[0]-1, head[1]) || map[head[0]-1][head[1]] == "*") 
-				console.log("Game Over!");
-			MoveDirection[0] = -1;
-			MoveDirection[1] = 0;
-			if(Transboundary(map, head[0]-1, head[1])&&
-				map[head[0]-1][head[1]] == "-" ||
-				map[head[0]-1][head[1]] == "0" ) {
-				var bodyEnd = body.pop();
-				eatFood(head[0]-1, head[1], bodyEnd);
-				body.unshift(new Array(head[0]-1, head[1]));
-				reDrawSnake(body[0], head, bodyEnd);
-			}
-			if(direction == "right") direction = "left";
-			break;
-		case "right": 
-			if(!Transboundary(map, head[0]+1, head[1]) || map[head[0]+1][head[1]] == "*") 
-				console.log("Game Over!");
-			MoveDirection[0] = 1;
-			MoveDirection[1] = 0;
-			if(Transboundary(map, head[0]+1, head[1])&&
-				map[head[0]+1][head[1]] == "-" ||
-				map[head[0]+1][head[1]] == "0" ) {
-				var bodyEnd = body.pop();
-				eatFood(head[0]+1, head[1], bodyEnd);
-				body.unshift(new Array(head[0]+1, head[1]));
-				reDrawSnake(body[0], head, bodyEnd);
-			}
-			if(direction == "left") direction = "right";
-			break;
+		case "up":config.updateMoveDires(0, -1); break;
+		case "down":config.updateMoveDires(0, 1); break;
+		case "left":config.updateMoveDires(-1, 0); break;
+		case "right":config.updateMoveDires(1, 0); break;
 	}
-	//console.log(body);
-	function eatFood(x, y, bodyEnd) {
-		//console.log(map[x][y]);
-		if(map[x][y] == "0") {
-			body.push(bodyEnd);
-			eatFood1(bodyEnd);
-			createFood();
-		}
+	let dires = config.MoveDires;
+	let x = head[0] + dires[0];
+	let y = head[1] + dires[1];
+	if(!isTransboundary(map, x, y) || this.GameMap[x][y]=="*")
+		console.log("Game Over!");
+	if(isTransboundary(map, x, y) && (map[x][y]=="-" || map[x][y]=="0")) {
+		let tail = (this.Body).pop();	// clear old tail;
+		this.eatFood(x, y, tail);
+		this.Body.unshift(new Array(x, y)); // insert new head;
+		this.reMap(new Array(x, y), tail); // update game map;
+		reDrawSnake(new Array(x, y), this.Body[1], tail);
 	}
-	/*function selfMove() {
-		if(Transboundary(map, head[0]+MoveDirection[0], head[1]+MoveDirection[1])&&
-			map[head[0]+MoveDirection[0]][head[1]+MoveDirection[1]] == "-" ||
-			map[head[0]+MoveDirection[0]][head[1]+MoveDirection[1]] == "0" ) {
-			var bodyEnd = body.pop();
-			eatFood(head[0]+MoveDirection[0], head[1]+MoveDirection[1], bodyEnd);
-			body.unshift(new Array(head[0]+MoveDirection[0], head[1]+MoveDirection[1]));
-			reDrawSnake(body[0], head, bodyEnd);
-		}
-	}*/
-	function Transboundary(map, x, y) {
-		if(x>=0 && x<map.length && y>=0 && y<map[0].length)
+	function isTransboundary(map, x, y) {
+		if(x>=0 && x<map.length && y>=0 && y<map[0].length) 
 			return true;
 		return false;
 	}
+	function reDrawSnake(newHead, oldHead, clearTail) {
+		draw.drawRole(newHead[0]+0.5, newHead[1]+0.5, 5, "red");
+		draw.drawRole(oldHead[0]+0.5, oldHead[1]+0.5, 5, "blue");
+		draw.clearRole(clearTail[0]+0.5, clearTail[1]+0.5);
+	}
 }
-var GameMap = function() {
-	let map = new Array();
-	for(let i=0; i<50; i++) {
-		map[i] = [];
-		for(let j=0; j<50; j++) {
-			map[i][j] = "-";
-		}
+Snake.prototype.eatFood = function(x, y, tail) {
+	if(this.GameMap[x][y] == "0") {
+		this.Body.push(tail);
+		draw.drawRole(tail[0]+0.5, tail[1]+0.5, 5, "blue");
+		this.createFood();	
 	}
-	
-	let canvas = document.getElementById("box");
-	let ctx    = canvas.getContext("2d");
-	var snake  = new Snake();
-	
-	this.getMap = function() {
-		return map;
-	}
-	this.DrawMap = function() {
-		let snakeXY = snake.InitSnakeBody();
-		for(let i=0; i<=50; i++) {
+}
+
+var Draw = function(ctx) {
+	this.drawMap = function(MapSize) {
+		for(let i=0; i<=MapSize; i++) {
 			ctx.moveTo(i*10, 0);
-			ctx.lineTo(i*10, 500);
+			ctx.lineTo(i*10, MapSize*10);
 			ctx.moveTo(0, i*10);
-			ctx.lineTo(500, i*10);
+			ctx.lineTo(MapSize*10, i*10);
 			ctx.stroke();
 		}
-		
-		map[snakeXY[0][0]][snakeXY[0][1]] = "*";
-		this.drawInitSnake(snakeXY[0][0]+0.5, snakeXY[0][1]+0.5, 5, "red");
-		for(let i=1; i<snakeXY.length; i++) {
-			let x = snakeXY[i][0]+0.5;
-			let y = snakeXY[i][1]+0.5;
-			this.drawInitSnake(x, y, 5, "blue");
-			map[snakeXY[i][0]][snakeXY[i][1]] = "*";
-		}
-		
-	}
-	this.drawInitSnake = function(x, y, r, color) {
-		ctx.beginPath();
-		ctx.arc(x*10,y*10, r, 0, 2*Math.PI);
-		ctx.fillStyle = color;
-		ctx.fill();
-		ctx.stroke();
-	}
-	this.clearSnake = function(x, y) {
-		ctx.beginPath();
-		ctx.clearRect((x-0.5)*10, (y-0.5)*10, 10, 10);
-		ctx.moveTo((x-0.5)*10, (y-0.5)*10);
-        ctx.lineTo((x-0.5)*10+10, (y-0.5)*10);
-        ctx.lineTo((x-0.5)*10+10, (y-0.5)*10+10);
-        ctx.lineTo((x-0.5)*10, (y-0.5)*10+10);
-        ctx.lineTo((x-0.5)*10, (y-0.5)*10);
-		ctx.stroke();
-	}
-	this.reMap = function(newHead, clearEnd) {
-		map[newHead[0]][newHead[1]]   = "*";
-		map[clearEnd[0]][clearEnd[1]] = "-";			
-	}
-	this.DrawSnake = function(SnakeBody) {
-		//var x = 
 	}
 }
-
-var gmap  = new GameMap();
-var snake = new Snake();
-gmap.DrawMap();
-gmap.DrawSnake();
-var m = gmap.getMap();
-
-setInterval("selfMove1()", 1);
-function selfMove1() {
-
-	if(MoveDirection[0]==-1 && MoveDirection[1]==0) direction = "left";
-	if(MoveDirection[0]==0 && MoveDirection[1]==-1) direction = "up";
-	if(MoveDirection[0]==1 && MoveDirection[1]==0) direction = "right";
-	if(MoveDirection[0]==0 && MoveDirection[1]==1) direction = "down";
-
-
-	var snake_body = snake.getSnakeBody();
-	//console.log("getbody", snake_body[0], foodPS);
-	//console.log("??", BFSPath(m, snake_body[0], foodPS));
-	snake.MoveSnake(m, BFSPath(m, snake_body[0], foodPS));
-
-	//snake.MoveSnake(m, direction);	
+Draw.prototype.drawRole = function(x, y, r, color) {
+	ctx.beginPath();
+	ctx.arc(x*10,y*10, r, 0, 2*Math.PI);
+	ctx.fillStyle = color;
+	ctx.fill();
+	ctx.stroke();
 }
+Draw.prototype.clearRole = function(x, y) {
+	ctx.beginPath();
+	ctx.clearRect((x-0.5)*10, (y-0.5)*10, 10, 10);
+	ctx.moveTo((x-0.5)*10, (y-0.5)*10);
+    ctx.lineTo((x-0.5)*10+10, (y-0.5)*10);
+    ctx.lineTo((x-0.5)*10+10, (y-0.5)*10+10);
+    ctx.lineTo((x-0.5)*10, (y-0.5)*10+10);
+    ctx.lineTo((x-0.5)*10, (y-0.5)*10);
+	ctx.stroke();
+}
+var canvas = document.getElementById("box");
+var ctx = canvas.getContext("2d");
 
-function reDrawSnake(newHead, oldHead, clearEnd) {
-	gmap.drawInitSnake(newHead[0]+0.5, newHead[1]+0.5, 5, "red");
-	gmap.drawInitSnake(oldHead[0]+0.5, oldHead[1]+0.5, 5, "blue");
-	gmap.clearSnake(clearEnd[0]+0.5, clearEnd[1]+0.5);
-	gmap.reMap(newHead, clearEnd);
-	//console.clear();
-	//showMap();
-}
-function eatFood1(bodyEnd) {
-	gmap.drawInitSnake(bodyEnd[0]+0.5, bodyEnd[1]+0.5, 5, "blue");
-}
+var config = new GameConfig();
+var snake  = new Snake();
+var draw   = new Draw(ctx);
 
-createFood();
-function createFood() {
-	if(foodPS.length != 0) {
-		gmap.clearSnake(foodPS[0]+0.5, foodPS[1]+0.5);
-		m[foodPS[0]][foodPS[1]] = "-";
-	}
-	foodPS[0] = parseInt((Math.random()*49).toString(10));
-	foodPS[1] = parseInt((Math.random()*49).toString(10));
-	
-	if(m[foodPS[0]][foodPS[1]] == "-") {
-		m[foodPS[0]][foodPS[1]] = "0";
-		gmap.drawInitSnake(foodPS[0]+0.5, foodPS[1]+0.5, 5, "yellow");
-		//console.log(foodPS[0], foodPS[1]);
-		//showMap();
-	}else
-		createFood();
+draw.drawMap(config.MapSize);
+snake.InitSnakeBody();
+snake.InitGameMap(config.MapSize);
+snake.InitSnake();
+snake.createFood();
+
+
+document.onkeydown = function(event) {
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if(e && e.keyCode == 37) snake.MoveSnake(snake.GameMap, "left");
+	if(e && e.keyCode == 38) snake.MoveSnake(snake.GameMap, "up");
+	if(e && e.keyCode == 39) snake.MoveSnake(snake.GameMap, "right");
+	if(e && e.keyCode == 40) snake.MoveSnake(snake.GameMap, "down");
 }
-$(document).ready(function() {
-	document.onkeydown = function(event) {
-		var e = event || window.event || arguments.callee.caller.arguments[0];
-		if(e && e.keyCode == 37) snake.MoveSnake(gmap.getMap(), "left");
-		if(e && e.keyCode == 38) snake.MoveSnake(gmap.getMap(), "up");
-		if(e && e.keyCode == 39) snake.MoveSnake(gmap.getMap(), "right");
-		if(e && e.keyCode == 40) snake.MoveSnake(gmap.getMap(), "down");
-	}
-});
-function showMap() {
-	for(let i=0; i<50; i++) {
-		let s = "";
-		for(let j=0; j<m[0].length; j++) {
-			s = s + m[j][i] ;
-		}
-		console.log(s+" "+i);
-	}
+setInterval("selfMove()", 50);
+function selfMove() {
+	snake.MoveSnake(snake.GameMap, BFSPath(snake.GameMap, snake.Body[0], snake.Food));
 }
-/*##############################################################*/
-var dires = [[-1,  0], [ 1,  0], [ 0, -1], [ 0,  1]];
 function BFSPath(map, snakePS, foodPS) {
 	var ps = BFS(map, snakePS, foodPS);
-
-	//console.log("----", ps);
 	ps[0] = ps[0] - snakePS[0];
 	ps[1] = ps[1] - snakePS[1];
-	//console.log("!!!", ps);
 	if(ps.length !=0) {
 		if(ps[0] == -1 && ps[1] ==0) return "left";
 		if(ps[0] == 1 && ps[1] ==0) return "right";
@@ -258,51 +159,47 @@ function BFSPath(map, snakePS, foodPS) {
 function BFS(map, snakePS, foodPS) {
 	var dires = [[-1,  0], [ 1,  0], [ 0, -1], [ 0,  1]];
 	var flag = new Array();
-	for(let i=0; i<50; i++) {
-		flag[i] = [];
-		for(let j=0; j<50; j++)
-			flag[i][j] = 0; 
+	for(let i=0; i<config.MapSize; i++) {
+		flag[i] = new Array(config.MapSize);
+		flag[i].fill(0);
 	}
 	var que = new Array();
 	que.push(new Array(snakePS[0], snakePS[1], 1));
 	while(que.length != 0) {
-		var ps = que.shift();
+		let ps = que.shift();
 		flag[ps[0]][ps[1]] = ps[2];
-		if(map[ps[0]][ps[1]] == "0"){
+		if(map[ps[0]][ps[1]] == "0")
 			return getPath(flag, ps[0], ps[1]);
-			break;
-		}
 		for(let i=0; i<dires.length; i++) {
-			if( Transboundary1(m, ps[0]+dires[i][0], ps[1]+dires[i][1]) &&
-				m[ps[0]+dires[i][0]][ps[1]+dires[i][1]] != "*" &&
-				flag[ps[0]+dires[i][0]][ps[1]+dires[i][1]] == 0 ) {
-
-				que.push(new Array(ps[0]+dires[i][0], ps[1]+dires[i][1], ps[2]+1));
-				flag[ps[0]+dires[i][0]][ps[1]+dires[i][1]] = ps[2] + 1;
+			let x = ps[0] + dires[i][0];
+			let y = ps[1] + dires[i][1];
+			if(Transboundary(map, x, y) && map[x][y]!="*" && flag[x][y]==0) {
+				que.push(new Array(x, y, ps[2]+1));
+				flag[x][y] = ps[2] + 1;
 			}
 		}
 	}
 }
-function getPath(nums, x, y) {
+function getPath(flag, x, y) {
 	var dires = [[-1,  0], [ 1,  0], [ 0, -1], [ 0,  1]];
-	var result = new Array();
 	var que = new Array();
-	que.push(new Array(x, y, nums[x][y]));
-	while(que.length != 0) {
-		var ps = que.shift();
+	que.push(new Array(x, y, flag[x][y]));
+	while(que.length) {
+		let ps = que.shift();
 		if(ps[2] == 2)
 			return ps;
 		for(let i=0; i<dires.length; i++) {
-			if( Transboundary1(nums, ps[0]+dires[i][0], ps[1]+dires[i][1]) &&
-				nums[ps[0]+dires[i][0]][ps[1]+dires[i][1]] == ps[2]-1 ) {
-				que.push(new Array(ps[0]+dires[i][0], ps[1]+dires[i][1], ps[2]-1));
-				break;			
+			let x = ps[0] + dires[i][0];
+			let y = ps[1] + dires[i][1];
+			if(Transboundary(flag, x, y) && flag[x][y]==ps[2]-1) {
+				que.push(new Array(x, y, ps[2]-1));
+				break;
 			}
 		}
 	}
 }
-function Transboundary1(map, x, y) {
-	if(x>=0 && x<map.length && y>=0 && y<map[0].length)
+function Transboundary(map, x, y) {
+	if(x>=0 && x<map.length && y>=0 && y<map[0].length) 
 		return true;
 	return false;
 }
